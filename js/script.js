6287c6acbde40e524ba888be6f840c353e5a286e
rajaalebchiri <rajaa.lebchiri@gmail.com>
2020-01-05 00:33:10 +0100

    j

--- js/script.js
+++ js/script.js
@@ -1,64 +1,30 @@
+new WOW().init();
 
-
-(function ($) {
-    'use strict';
-AOS.init();
-
-    // ----------------------- 
-        // Progress Bar--------------------
-        // 
-        // 
-
-    $(window).on ('load', function (){ 
-          
-        $('.progress-bar').each(function(){
-                var width = $(this).data('percent');
-                $(this).css({'transition': 'width 3s'});
-                $(this).appear(function() {
-                    console.log('hello');
-                    $(this).css('width', width + '%');
-                    $(this).find('.count').countTo({
-                        from: 0,
-                        to: width,
-                        speed: 3000,
-                        refreshInterval: 50
-                    });
-                });
-            });
-        }); 
-
-    $('.owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        dots:false,
-        autoplayTimeout:8000
+$(function () {
+    var selectedClass = "";
+    $(".filter").click(function () {
+      selectedClass = $(this).attr("data-rel");
+      $("#gallery").fadeTo(100, 0.1);
+      $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+      setTimeout(function () {
+        $("." + selectedClass).fadeIn().addClass('animation');
+        $("#gallery").fadeTo(300, 1);
+      }, 300);
     });
+  });
 
-    // Shuffle js filter and masonry
-    var Shuffle = window.Shuffle;
-    var jQuery = window.jQuery;
-
-    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-        itemSelector: '.shuffle-item',
-        buffer: 1
-    });
-
-    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-        var input = evt.currentTarget;
-        if (input.checked) {
-            myShuffle.filter(input.value);
-        }
-    });
-
-     $('.portfolio-gallery').each(function () {
-        $(this).find('.popup-gallery').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    });
-
-
-})(jQuery);
+  $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  
+    for (var i = 0; i < 4; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
